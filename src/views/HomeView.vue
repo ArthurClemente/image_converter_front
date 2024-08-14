f13823673501270762b3494daa99ab01c462c549
Arthur Clemente <arthurclementte@hotmail.com>
2024-08-13 21:40:28 -0300

    refactor: ImageInput component imported and style created for the home view.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
+import ImageInput from '@/components/ImageInput.vue';
 </script>
 
 <template>
   <main id="mainContent">
-    <section class="hederArea">
+    <section id="hederArea">
       <Header />
     </section>
-    <section class="contentArea">
-      
+    <section id="imageConverterArea">
+      <ImageInput />
     </section>
-    <section class="footerArea">
-
-    </section>
+    <section id="footerArea"></section>
   </main>
 </template>
+
+<style scoped>
+  #mainContent {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  #imageConverterArea {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    background-color: #023e8a;
+  }
+
+  .inputZone {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
